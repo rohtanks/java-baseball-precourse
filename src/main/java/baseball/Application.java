4a06c1f67125c1b2441419fa460857323feb86b7
arroh <arroh@12cm.co.kr>
2022-04-15 11:52:01 +0900

    feat: 게임 로직 구현
    main 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import baseball.domain.game.GameControll;
+import baseball.domain.game.Judgement;
+import baseball.domain.number.BaseballNumber;
+import baseball.domain.user.UserInput;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseballNumber baseballNumber = new BaseballNumber();
+        UserInput userInput = new UserInput();
+        Judgement judgement = new Judgement();
+        GameControll gameControll = new GameControll(baseballNumber, userInput, judgement);
+        while (!"2".equals(gameControll.start())) {
+        }
+        System.out.println("게임 종료");
     }
 }
